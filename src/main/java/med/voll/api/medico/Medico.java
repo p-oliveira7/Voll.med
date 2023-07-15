ea57e233b87cedae0c44764257e6c80a63a53183
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-15 01:17:03 -0300

    Feat: mudando o metodo excluir para exclusão lógica

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -26,7 +26,10 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -45,6 +48,10 @@ public class Medico {
         if (dados.endereco() != null) {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
 
