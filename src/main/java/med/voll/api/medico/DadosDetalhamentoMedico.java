51594afb00d0f180e0c4e9778f65f615cdafbcb3
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-15 16:47:18 -0300

    Fix: Campos faltando no detalhamento de medico

--- src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -6,10 +6,11 @@ public record DadosDetalhamentoMedico(
         Long id,
         String nome,
         String email,
+        String telefone,
         String crm,
         Especialidade especialidade,
         Endereco endereco) {
     public DadosDetalhamentoMedico(Medico medico){
-        this(medico.getId(), medico.getNome(), medico.getCrm(), medico.getEmail(), medico.getEspecialidade(), medico.getEndereco());
+        this(medico.getId(),  medico.getNome(),medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
     }
 }
