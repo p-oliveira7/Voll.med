cce0d19e4816b932962c72ec52f41ac34fa5f67c
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-15 16:14:22 -0300

    Feat: padronizando os retornos http dos metodos GET, PUT, DELETE

---
+++ src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,15 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade,
+        Endereco endereco) {
+    public DadosDetalhamentoMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getCrm(), medico.getEmail(), medico.getEspecialidade(), medico.getEndereco());
+    }
+}
