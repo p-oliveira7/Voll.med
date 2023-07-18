836cb884fb02a15b0ad4cad2cbd801d976d7cb6c
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-18 18:38:24 -0300

    Fix: tratando 403 por horario errado na data de experação do token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -34,7 +34,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-
         filterChain.doFilter(request, response);
     }
 
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,7 +1,6 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
@@ -34,18 +33,19 @@ public class TokenService {
     }
 
     public String getSubject(String tokenJWT) {
+        System.out.println(tokenJWT);
         try {
-            var algoritmo = Algorithm.HMAC256(secret);
-            return JWT.require(algoritmo)
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
                     .withIssuer(ISSUER)
                     .build()
                     .verify(tokenJWT)
                     .getSubject();
         } catch (JWTVerificationException exception) {
-            throw new RuntimeException("Token JWT inválido ou expirado!" + tokenJWT);
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
     private Instant dataExpiracao(){
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("+03:00"));
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
