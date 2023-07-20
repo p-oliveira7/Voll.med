57ba1ec1a9f81ab991d10c7f47951206f2736acf
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-20 15:25:42 -0300

    Configurando SpringDoc

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -4,12 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 
     @Query("select p.ativo from Paciente p where p.id = :id")
-    Boolean findAtivoById(@Param("id") Long id);
+    Boolean findAtivoById(Long id);
 
 }
